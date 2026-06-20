2762b49340088c0383250c4268e5a93a72b69cc4
Lemonsaurus <2098517+lemonsaurus@users.noreply.github.com>
2026-06-20 11:27:00 +0200

    Add repaint loop

--- main.zig
+++ main.zig
@@ -1,6 +1,20 @@
 const std = @import("std");
+const win = std.os.windows;
 
-pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("john was afraid\n", .{});
+const BOOL = win.BOOL;
+const DWORD = win.DWORD;
+
+extern "user32" fn InvalidateRect(hWnd: ?win.HWND, lpRect: ?*const anyopaque, bErase: BOOL) callconv(.winapi) BOOL;
+extern "kernel32" fn Sleep(dwMilliseconds: DWORD) callconv(.winapi) void;
+
+pub export fn wWinMain(
+    _: ?win.HINSTANCE,
+    _: ?win.HINSTANCE,
+    _: ?win.LPWSTR,
+    _: c_int,
+) callconv(.winapi) c_int {
+    while (true) {
+        _ = InvalidateRect(null, null, 0);
+        Sleep(100);
+    }
 }

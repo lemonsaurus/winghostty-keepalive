2e7e5de4350ff51614b338cc579a3a1e4f5359a9
Lemonsaurus <2098517+lemonsaurus@users.noreply.github.com>
2026-06-20 11:42:00 +0200

    Enumerate windows before repainting

--- main.zig
+++ main.zig
@@ -1,11 +1,19 @@
 const std = @import("std");
 const win = std.os.windows;
 
+const HWND = win.HWND;
 const BOOL = win.BOOL;
 const DWORD = win.DWORD;
+const LPARAM = win.LPARAM;
 
-extern "user32" fn InvalidateRect(hWnd: ?win.HWND, lpRect: ?*const anyopaque, bErase: BOOL) callconv(.winapi) BOOL;
+extern "user32" fn InvalidateRect(hWnd: ?HWND, lpRect: ?*const anyopaque, bErase: BOOL) callconv(.winapi) BOOL;
+extern "user32" fn EnumWindows(lpEnumFunc: *const fn (HWND, LPARAM) callconv(.winapi) BOOL, lParam: LPARAM) callconv(.winapi) BOOL;
 extern "kernel32" fn Sleep(dwMilliseconds: DWORD) callconv(.winapi) void;
+
+fn enumTopCb(hwnd: HWND, _: LPARAM) callconv(.winapi) BOOL {
+    _ = InvalidateRect(hwnd, null, 0);
+    return 1;
+}
 
 pub export fn wWinMain(
     _: ?win.HINSTANCE,
@@ -14,7 +22,7 @@ pub export fn wWinMain(
     _: c_int,
 ) callconv(.winapi) c_int {
     while (true) {
-        _ = InvalidateRect(null, null, 0);
+        _ = EnumWindows(&enumTopCb, 0);
         Sleep(100);
     }
 }
